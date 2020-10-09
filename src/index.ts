18b75a366830764e09d86c392b04e6244a958e02
Anthony Fu <anthonyfu117@hotmail.com>
2020-10-10 00:13:06 +0800

    feat: add random colors

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,31 @@ export interface Color {
   lightness: number
 }
 
+function shuffle<T>(_arr: T[]): T[] {
+  const array = Array.from(_arr)
+  let currentIndex = array.length
+  let temporaryValue
+  let randomIndex
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
 export const colors = _colors as Color[]
 
 export function getColors(name: string) {
+  if (name === 'random')
+    return shuffle(colors)
   return colors.filter(i => i.names.includes(name))
 }
